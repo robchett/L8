fa480022fa77e7f9036d4d029060f2f209bbf97b
Ben Gosney <ben@evosite.co.uk>
2015-06-25 20:32:09 +0100

    Working Json api and a basic front end

--- w8.py
+++ w8.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 import json
+import hashlib
 
 from data.data import Data
 from data.config import args
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-
-errors_obj = Data(args)
 
 def list_routes():
     import urllib
@@ -34,28 +33,40 @@ def api():
             'methods' : list_routes()
             })
 
-@app.route(base_path + 'domains/')
+@app.route(base_path + 'domain/list/')
 def domains():
     """
     List the domains that have errors
     """
+    errors_obj = Data(args)
     domains = errors_obj.domains()
     json_domains = []
 
     for d in domains:
         json_domains.append(d.__dict__)
 
-    return jsonify({'domains':json_domains})
+    return jsonify({'domains':json_domains, 'hash': hashlib.md5(str(json_domains)).hexdigest()})
 
 
-@app.route(base_path + 'errors/<string:host>/<string:mode>/')
-def errors(host, mode):
+@app.route(base_path + 'domain/<string:host>/errors/')
+@app.route(base_path + 'domain/<string:host>/errors/<string:mode>/')
+def errors(host, mode=1):
     """
     List the errors for a domain
     """
 
-    return jsonify(errors_obj.errors(host, mode))
+    errors_obj = Data(args)
+    errs = errors_obj.errors(host, mode)
+    json_errs = []
 
+    for e in errs:
+        json_errs.append(e.__dict__)
+
+    return jsonify({'errors':json_errs, 'hash': hashlib.md5(str(json_errs)).hexdigest()})
+
+@app.route('/')
+def template():
+    return open('template.html').read(100000)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
