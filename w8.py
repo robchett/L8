c3023158c8d9df8014c5c56b3b69969ca93d311b
Ben Gosney <ben@evosite.co.uk>
2015-06-29 08:54:50 +0100

    Moved date formatting to the back end and added auto error refresh

--- w8.py
+++ w8.py
@@ -22,6 +22,49 @@ def list_routes():
 
     return output
 
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + " months ago"
+    return str(day_diff / 365) + " years ago"
+
 base_path = '/api/v1.0/'
 
 @app.route(base_path)
@@ -65,6 +108,7 @@ def errors(host, mode=1):
     for e in errs:
         err_dict = e.__dict__
         #err_dict['timestamp'] = int(time.mktime(err_dict['time'].timetuple()))
+        err_dict['ptime'] = pretty_date(err_dict['time'])
         json_errs.append(err_dict)
 
     return jsonify({'errors':json_errs, 'hash': hashlib.md5(str(json_errs)).hexdigest()})
