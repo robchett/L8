e60966f8a4943da1d3bf8cb8a600e497dd6155c8
Rob Chettleburgh <rob@evosite.co.uk>
2016-02-18 13:24:30 +0000

    Catch decoding errors on message context

--- data/subscriber.py
+++ data/subscriber.py
@@ -2,6 +2,7 @@ import json
 import redis
 import re  # regular expressions
 import base64
+from bcolors import bcolors
 
 
 class Subscriber:
@@ -14,9 +15,12 @@ class Subscriber:
         for item in self.pubsub.listen():
             record = json.loads(item['data'])
             if decode:
-                regex = re.compile('^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$')
-                if regex.match(record['context']):
-                    record['context'] = base64.b64decode(record['context'])
+                try:
+                    regex = re.compile('^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$')
+                    if regex.match(record['context']):
+                        record['context'] = base64.b64decode(record['context'])
+                except TypeError:
+                    bcolors.print_colour("Failed to decode message %s\n" % record['context'], bcolors.WARNING, bcolors.BOLD)
             callable(record)
 
     def work_raw(self, callable):
