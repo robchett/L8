6be0bb5ac098524f2198e131f7f81c19b3a43f32
Rob Chettleburgh <rob@evosite.co.uk>
2018-02-20 14:38:10 +0000

    Better config for w8

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@
 python setup.py install
 
 function create_service() {
-	echo "Creating service $1"
-	touch /etc/systemd/system/$1.service
-	chmod 664 /etc/systemd/system/$1.service
-	cat << SERVICE > /etc/systemd/system/$1.service
+        echo "Creating service $1"
+        touch /etc/systemd/system/$1.service
+        chmod 664 /etc/systemd/system/$1.service
+        cat << SERVICE > /etc/systemd/system/$1.service
 [Unit]
 Description=$1
 After=network.target redis.service mariadb.service
 
 [Service]
-ExecStart=/usr/bin/python $2/$1.py
+ExecStart=$2
 Type=simple
 
 [Install]
@@ -20,8 +20,33 @@ WantedBy=default.target
 SERVICE
 }
 
-create_service s8 `pwd`
+function create_uwsgi_service() {
+        echo "Creating service $1"
+        touch /etc/systemd/system/$1.service
+        chmod 664 /etc/systemd/system/$1.service
+        cat << SERVICE > /etc/systemd/system/$1.service
+[Unit]
+Description=$1
+After=network.target redis.service mariadb.service
 
-create_service a8 `pwd`
+[Service]
+ExecStart=/usr/sbin/uwsgi --ini $2
+RuntimeDirectory=uwsgi
+Restart=always
+KillSignal=SIGQUIT
+Type=notify
+StandardError=syslog
+NotifyAccess=all
 
-create_service p8 `pwd`
+
+[Install]
+WantedBy=default.target
+SERVICE
+}
+
+
+create_service s8 "/usr/bin/python $(pwd)/s8.py"
+create_service a8 "/usr/bin/python $(pwd)/a8.py"
+create_service p8 "/usr/bin/python $(pwd)/p8.py"
+create_uwsgi_service w8 "$(pwd)/w8.ini"
+
